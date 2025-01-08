0a55763da0422e566965d12b16b5759c797106dc
Daniel Csicsak <csicsak.daniel@gmail.com>
2025-01-08 18:07:40 +0100

    Starting CPP

---
+++ CommonCore/CPP0/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		if (i > 1 && argv[i][0] && argv[i][0] != ' '
+			&& argv[i - 1][0] != ' ')
+			std::cout << " ";
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)toupper(argv[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
