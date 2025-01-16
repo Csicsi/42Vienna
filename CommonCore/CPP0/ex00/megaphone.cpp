3bb6cf0a80bd728bd497562de4d9b44a26525bbf
Csicsi <csicsak.daniel@gmail.com>
2025-01-16 09:47:49 +0100

    removed unnecessary header

--- CommonCore/CPP0/ex00/megaphone.cpp
+++ CommonCore/CPP0/ex00/megaphone.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <cctype>
 
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 		return (0);
 	}
 	for (int i = 1; i < argc; i++)
