211f8a617faa0334091254564b5640fc0ebd5383
Daniel Csicsak <csicsak.daniel@gmail.com>
2025-03-18 17:10:24 +0100

    isX functions improved, converter

--- CommonCore/CPP/CPP06/ex00/includes/Converter.hpp
+++ CommonCore/CPP/CPP06/ex00/includes/Converter.hpp
@@ -2,10 +2,12 @@
 #define CONVERTER_HPP
 
 #include <iostream>
-#include <climits>
-#include <cstdlib>
 #include <cfloat>
 #include <sstream>
+#include <climits>
+#include <cstdlib>
+#include <cerrno>
+#include <limits>
 
 class Converter {
 private:
@@ -13,13 +15,13 @@ private:
 	~Converter();
 	Converter(const Converter &other);
 	Converter &operator=(const Converter &other);
-
-	static bool isChar(std::string const &literal);
-	static bool isInt(std::string const &literal);
-	static bool isFloat(std::string const &literal);
-	static bool isDouble(std::string const &literal);
 public:
 	static void convert(std::string const &literal);
 };
+
+bool isChar(std::string const &literal);
+bool isInt(std::string const &literal);
+bool isFloat(std::string const &literal, bool recursive = false);
+bool isDouble(std::string const &literal);
 
 #endif
--- CommonCore/CPP/CPP06/ex00/srcs/Converter.cpp
+++ CommonCore/CPP/CPP06/ex00/srcs/Converter.cpp
@@ -20,43 +20,124 @@ Converter &Converter::operator=(const Converter &other) {
 	return *this;
 }
 
-bool Converter::isChar(const std::string &literal) {
-	return (literal.length() == 1 && !isdigit(literal[0]));
+bool isChar(const std::string &literal) {
+	return (literal.length() == 1 && !isdigit(literal[0]) && isprint(literal[0]));
 }
 
-bool Converter::isInt(const std::string &literal) {
-	std::stringstream ss(literal);
-	unsigned int i;
+bool isInt(const std::string &literal) {
+	if (literal.empty())
+		return false;
 
-	ss >> i;
-	return (ss.eof() && !ss.fail());
+	char *endptr;
+	errno = 0;
+	long l = std::strtol(literal.c_str(), &endptr, 10);
+
+	if (*endptr != '\0' || errno == ERANGE || l < INT_MIN || l > INT_MAX)
+		return false;
+	return true;
 }
 
-bool Converter::isFloat(const std::string &literal) {
-	std::stringstream ss(literal);
-	float f;
+bool isFloat(const std::string &literal, bool recursive) {
+	if (literal.empty() || literal == "f")
+		return false;
+	if (literal[literal.length() - 1] == 'f' && !recursive) {
+		return isFloat(literal.substr(0, literal.length() - 1), true);
+	}
+	char *endptr;
+	errno = 0;
+	float f = std::strtof(literal.c_str(), &endptr);
+	if (*endptr != '\0' || errno == ERANGE)
+		return false;
+	(void)f;
 
-	ss >> f;
-	return (ss.eof() && !ss.fail());
+	double d = std::strtod(literal.c_str(), &endptr);
+	if ((literal != "inf" && literal != "-inf" && literal != "+inf")
+		&& (d < -std::numeric_limits<float>::max()
+		|| d > std::numeric_limits<float>::max()))
+		return false;
+
+	return true;
 }
 
-bool Converter::isDouble(const std::string &literal) {
-	std::stringstream ss(literal);
-	double d;
+bool isDouble(const std::string &literal) {
+	if (literal.empty())
+		return false;
 
-	ss >> d;
-	return (ss.eof() && !ss.fail());
+	char *endptr;
+	errno = 0;
+	double d = std::strtod(literal.c_str(), &endptr);
+	(void)d;
+
+	if (*endptr != '\0' || errno == ERANGE)
+		return false;
+
+	return true;
 }
+
 
 void Converter::convert(const std::string &literal) {
-	if (isChar(literal))
-		std::cout << "char" << std::endl;
-	else if (isInt(literal))
-		std::cout << "int" << std::endl;
-	else if (isFloat(literal))
-		std::cout << "float" << std::endl;
-	else if (isDouble(literal))
-		std::cout << "double" << std::endl;
+	char c;
+	int i;
+	float f;
+	double d;
+
+	if (isChar(literal)) {
+		c = literal[0];
+		i = static_cast<int>(c);
+		f = static_cast<float>(c);
+		d = static_cast<double>(c);
+	}
+	else if (isInt(literal)) {
+		i = std::atoi(literal.c_str());
+		c = (i >= 32 && i <= 126) ? static_cast<char>(i) : 0;
+		f = static_cast<float>(i);
+		d = static_cast<double>(i);
+	}
+	else if (isFloat(literal)) {
+		f = std::strtof(literal.c_str(), NULL);
+		i = static_cast<int>(f);
+		c = (i >= 32 && i <= 126) ? static_cast<char>(i) : 0;
+		d = static_cast<double>(f);
+	}
+	else if (isDouble(literal)) {
+		d = std::strtod(literal.c_str(), NULL);
+		i = static_cast<int>(d);
+		c = (i >= 32 && i <= 126) ? static_cast<char>(i) : 0;
+		f = static_cast<float>(d);
+	}
+	else {
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: impossible" << std::endl;
+		std::cout << "double: impossible" << std::endl;
+		return;
+	}
+
+	if (isprint(c))
+		std::cout << "char: '" << c << "'" << std::endl;
+	else if (isInt(literal) || isFloat(literal) || isDouble(literal))
+		std::cout << "char: Non displayable" << std::endl;
 	else
-		std::cout << "impossible" << std::endl;
+		std::cout << "char: impossible" << std::endl;
+
+	if (isInt(literal) || isFloat(literal) || isDouble(literal))
+		std::cout << "int: " << i << std::endl;
+	else
+		std::cout << "int: impossible" << std::endl;
+
+	if (isFloat(literal) || isInt(literal) || isDouble(literal)) {
+		std::cout << "float: " << f;
+		if (f == static_cast<int>(f))
+			std::cout << ".0";
+		std::cout << "f" << std::endl;
+	} else
+		std::cout << "float: impossible" << std::endl;
+
+	if (isDouble(literal) || isFloat(literal) || isInt(literal)) {
+		std::cout << "double: " << d;
+		if (d == static_cast<int>(d))
+			std::cout << ".0";
+		std::cout << std::endl;
+	} else
+		std::cout << "double: impossible" << std::endl;
 }
